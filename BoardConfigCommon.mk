96f9386b6ecea040ae55ba33b9f3b6e0b15e0cfc
klozz (TeamMEX) <xxx.reptar.rawrr.xxx@gmail.com>
2014-09-01 18:39:08 -0500

    Qcom optimizations, max egl caches, alow iON support

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,6 +29,8 @@ TARGET_BOARD_PLATFORM 		:= msm8226
 TARGET_BOOTLOADER_BOARD_NAME 	:= MSM8226
 TARGET_CPU_VARIANT 		:= krait
 TARGET_USE_KINGFISHER_OPTIMIZATION := true
+TARGET_USES_ION := true
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 -include device/motorola/qcom-common/BoardConfigCommon.mk
 
@@ -104,3 +106,13 @@ BOARD_RECOVERY_SWIPE		:= true
 
 #Temporary hack : skip building external/chromium_org
 PRODUCT_PREBUILT_WEBVIEWCHROMIUM := yes
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
